a6596d054b2bdbe912f4814467167a8452e3ccd2
Amelia Hardjasa <ahardjasa@gmail.com>
2015-02-21 14:12:19 -0800

    foodtrucks

---
+++ data/foodtruck_parsing.R
@@ -0,0 +1,10 @@
+foodtruck <- fread("C:/home/code2015datasets//new_food_vendor_locations.csv")
+simple <-foodtruck[status == "open", list(lat, lon, business_name, description)]
+simplemap <- data.table(term = c("taco", "sandwich", "crepe", "juice.*smoothie|smoothie.*juice"), name = c("Tacos", "Sandwiches", "Crepes", "Juice and Smoothies"))
+
+simple[, foodtype := gsub("^ +| $", "", gsub("Cuisine|Foods?", "", description))]
+for (i in 1:nrow(simplemap)){
+  simple[, foodtype := ifelse(grepl(simplemap[i, term], foodtype, ignore.case = T), simplemap[i, name], foodtype)]
+}
+simple[, description := NULL]
+write.csv(simple, "C:/cygwin64/home/amelia.hardjasa/code2015/data/foodtrucks.csv", row.names = F)
