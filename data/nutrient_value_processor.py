cbd2adce7f841c26c4cdaee99e4963de65233164
Neil Gentleman <ngentleman@gmail.com>
2015-02-20 23:11:31 -0800

    all the foods

--- data/nutrient_value_processor.py
+++ data/nutrient_value_processor.py
@@ -14,25 +14,36 @@ def clean(text):
 
 
 def parse(input_file, output_file=None):
+    name = None
+    columns = None
+    units = None
     foods = []
 
     with codecs.open(input_file, encoding='latin_1') as ifh:
         reader = csv.reader(ifh)
 
         for i, row in enumerate(reader):
-            if i == 2:
+            if row[0].startswith('Nutrient Value of Some Common Foods'):
+                continue
+            elif not name:
                 name = row[0]
-            elif i == 3:
+            elif not columns:
+                assert(row[1] == 'Measure')
                 columns = row
-            elif i == 4:
-                for c, unit in enumerate(row):
-                    columns[c] = (columns[c], unit)
+            elif not units and row[2] == 'g':
+                units = row
             elif row[4]:
                 foods.append([clean(c) for c in row])
 
+    if not units:
+        cols_with_unit = [(c, None) for c in columns]
+    else:
+        cols_with_unit = [(columns[c], unit) for c, unit in enumerate(units)]
+
     if output_file:
         with open(output_file, 'w') as ofh:
-            json.dump(dict(name=name, columns=columns, foods=foods), ofh, sort_keys=True)
+
+            json.dump(dict(name=name, columns=cols_with_unit, foods=foods), ofh, sort_keys=True)
 
 if __name__ == '__main__':
     parse(*sys.argv[1:])
