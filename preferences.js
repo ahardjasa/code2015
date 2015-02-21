d5f145f4a19d59e100b6e8a48690c77a7d1cae82
Ivan Zverev <ivan.zverev@gmail.com>
2015-02-21 15:29:36 -0800

    graphs!

--- preferences.js
+++ preferences.js
@@ -13,8 +13,33 @@ function User(first, last)
 	self.foodItems = [];
 
 	self.addFoodItem = function(day, foodItem) {
-		self.foodItems.push({Day:day, Food:foodItem});
+		var strippedDate = new Date(day.year, day.month, day.day, 0, 0, 0);
+		var food = self.foodItems.find(
+			function(element, index, array) {
+				return element.year === strippedDate.year && element.month === strippedDate.month
+					&& element.day === strippedDate.day; } );
+		if( food === undefined)
+			{self.foodItems.push({Day:strippedDate, Food:foodItem});}
+		else
+		{ food.Food += foodItem;}
+	};
+
+	self.getDataByFoodParamAndDate = function(paramName, day)
+	{
+		var strippedDate = new Date(day.year, day.month, day.day, 0, 0, 0);
+		var foodItemsForDay = self.foodItems[strippedDate];
+		if(foodItemsForDay !== undefined)
+		{
+			return foodItemsForDay[paramName];
+		}
+
+	};
+
+	self.SampleUser = function()
+	{
+
 	}
+
 }
 function UserPreferences()
 {
@@ -29,6 +54,11 @@ function UserPreferences()
 	self.dailyCalories = ko.observable(9000);
 	self.dailyIron = ko.observable(900);
 	self.dailyMagnesium = ko.observable(90);
+
+	self.isShowHistCalories = ko.observable("true");
+	self.isShowHistCarbs = ko.observable("true");
+	self.isShowHistProtein = ko.observable("true");
+	self.isShowHistFat = ko.observable("true");
 }
 
 function PreferencesViewModel()
@@ -50,6 +80,43 @@ function PreferencesViewModel()
 	{
 		$(function() {
 
+			// insert checkboxes
+			var displayTypes = $("#prefHistoryChoices");
+
+			displayTypes.find("input").click(plotAccordingToChoices);
+
+			function plotAccordingToChoices() {
+
+				var data = [];
+
+				if(self.myPreferences.isShowHistCalories)
+				{ data.push({label:"Calories", data: [[1,2]]}) ;}
+
+				if(self.myPreferences.isShowHistFat)
+				{ data.push({label:"Fat", data: [[3,4]]}) ;}
+
+				if(self.myPreferences.isShowHistProtein)
+				{ data.push({label:"Protein", data: [[5,6]]}) ;}
+
+				if(self.myPreferences.isShowHistCarbs)
+				{ data.push({label:"Carbs", data: [[7,8]]}) ;}
+
+				if(data.length > 0)
+				{$.plot("#prefHistoryPlot", data);}
+
+
+
+			}
+
+			plotAccordingToChoices();
+
+		});
+	};
+
+	self.DrawDayGraph = function()
+	{
+		$(function() {
+
 			var d1 = [];
 			for (var i = 0; i < 14; i += 0.5) {
 				d1.push([i, Math.sin(i)]);
@@ -61,15 +128,26 @@ function PreferencesViewModel()
 
 			var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
 
-			$.plot("#placeholder", [ d1, d2, d3 ]);
 
-			// Add the Flot version string to the footer
+			yaxis = {
+				show: false};
 
-			$("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
+			xaxis=  {
+				show: false};
+
+			grid = { show: false};
+
+			series = {
+				bars: {
+					show: true
+				}};
+
+			var options = {
+				yaxis: yaxis, xaxis: xaxis, grid: grid, series: series
+			};
+
+				$.plot("#prefDayPlot", [d1, d2, d3], options);
 		});
-
-
-
 	};
 }
 
