80e28feead487120e9e0b8d2640df8bd1ed37d03
Neil Gentleman <ngentleman@gmail.com>
2015-02-20 18:50:14 -0800

    hook up total calories

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-var model = {
-	increment: function () {
-		this.totalCalories(this.totalCalories() + 1);
-	},
-	totalCalories: ko.observable(9000)
+function FoodItem(source, name, calories) {
+	this.source = source;
+	this.name = name;
+	this.calories = calories;
+}
+
+function PageModel() {
+	this.foodItems = ko.observableArray([
+		new FoodItem("subway", "12\" meatball", 100)
+	]);
+	this.totalCalories = ko.computed(function () {
+		return this.foodItems().reduce(function (total, item) {
+			return total + item.calories;
+		}, 0);
+	}, this);
+}
+
+PageModel.prototype.addRandomFood = function () {
+	this.foodItems.push(new FoodItem("asdf", "cheese", 1000));
 };
+
+var model = new PageModel();
 
 $(function () {
 	ko.applyBindings(model);
