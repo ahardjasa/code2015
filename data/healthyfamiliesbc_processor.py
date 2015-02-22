9dd4f5c9d5da00fa188260e8e65b46773c796128
Neil Gentleman <ngentleman@gmail.com>
2015-02-21 19:31:53 -0800

    one script to build all the locations

---
+++ data/healthyfamiliesbc_processor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import codecs
+import csv
+import json
+
+
+def clean(text):
+    text = text.strip()
+    try:
+        return float(text)
+    except Exception:
+        return text
+
+
+def main():
+    with codecs.open('healthyfamiliesbc.csv', encoding='latin_1') as ifh:
+        reader = csv.reader(ifh)
+
+        for i, row in enumerate(reader):
+            if i == 0:
+                print('{"cols": ' + json.dumps(row) + ',\n"rows": [')
+            else:
+                comma = ', '
+                if i == 1:
+                    comma = '  '
+                print(comma + json.dumps([clean(c) for c in row]))
+        print(']}')
+
+if __name__ == '__main__':
+    main()
