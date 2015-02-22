e3928d01cc07f7ced2cb26fe6a5d560daefe90ef
Amelia Hardjasa <ahardjasa@gmail.com>
2015-02-22 00:59:55 -0800

    grilled cheeeese

--- data/menus.R
+++ data/menus.R
@@ -1,11 +1,20 @@
+require(data.table)
+source("C:/cygwin64/home/amelia.hardjasa/code2015/data/createMeal.R")
+
 nut2008 <- fread("C:/cygwin64/home/amelia.hardjasa/code2015/data/combined_nutrient_value_2008.csv")
 setkeyv(nut2008, names(nut2008)[1])
 
-menu1 <- data.table(foodtype = c("Hot Dogs"), mealname = c("Hotdog, beef wiener + condiments"), generic = c("Wiener (frankfurter), beef", "Roll, hamburger or hotdog, white", "Ketchup", "Mustard"))
+menu <- rbind(data.table(foodtype = c("Hot Dogs"), 
+                         mealname = c("Hotdog, beef wiener + condiments"), 
+                         generic = c("Wiener (frankfurter), beef", "Roll, hamburger or hotdog, white", "Ketchup", "Mustard")),
+              data.table(foodtype = "Sandwiches", 
+                         mealname = nut2008[grepl("sandwich", get("Food Name ()"), ignore.case = T) & SUBCATEGORY == "SANDWICHES", get("Food Name ()")], 
+                         generic = nut2008[grepl("sandwich", get("Food Name ()"), ignore.case = T) & SUBCATEGORY == "SANDWICHES", get("Food Name ()")]),
+              data.table(foodtype = c("Grilled Cheese Sandwiches"),
+                         mealname = c(rep("Grilled cheese, basic", 3), rep("Grilled cheese, fancy", 4)),
+                         generic = c("Bread, white, commercial", "Bread, white, commercial", "Cheddar", "Gruyere", "Bread, rye", "Bread, rye", "Bacon, pork, broiled, pan-fried or roasted")))
 
-menu2 <- data.table(foodtype = "Sandwiches", mealname = nut2008[grepl("sandwich", get("Food Name ()"), ignore.case = T) & SUBCATEGORY == "SANDWICHES", get("Food Name ()")], generic = nut2008[grepl("sandwich", get("Food Name ()"), ignore.case = T) & SUBCATEGORY == "SANDWICHES", get("Food Name ()")])
 
-menu <- rbind(menu1, menu2)
 meals <- menu[, createMeal(mealname, generic, nut2008), by = mealname]
 meals <- meals[, -1, with = F]
 export <- merge(menu, meals, by = "mealname", all = T)
