62dcf04682035c6814f98f26cf484caebc694de8
Neil Gentleman <ngentleman@gmail.com>
2015-03-11 12:05:29 -0700

    deploy: write CNAME for custom domain

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@ git checkout master
 git branch -D gh-pages
 git checkout -b gh-pages
 git filter-branch -f --subdirectory-filter public/ --prune-empty HEAD
+echo neighbourfood.ca > CNAME
+git add CNAME
+git commit -m CNAME
 git push -f origin gh-pages
 git checkout master
