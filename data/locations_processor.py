fb6ba628e6d9f18c9f220c64ac1569de2ac93bae
Neil Gentleman <ngentleman@gmail.com>
2015-03-11 21:06:42 -0700

    add some restaurant aliases

--- data/locations_processor.py
+++ data/locations_processor.py
@@ -8,11 +8,26 @@ def main():
     with open('fatabase.txt') as fb:
         fatabase_names = set(json.loads(r)[0] for r in fb)
         fatabase_names.remove('Restaurant')
+        fatabase_names = dict((n, n) for n in fatabase_names)
+        fatabase_names["Church's Fried Chicken"] = "Church's Chicken"
+        fatabase_names['COBS Bread'] = 'Cobs Bread'
+        fatabase_names['Freshslice'] = 'FreshSlice'
+        fatabase_names['De Dutch'] = 'DeDutch'
+        fatabase_names["Denny's"] = 'Dennys'
+        fatabase_names["Earl's Restaurant"] = 'Earls'
+        fatabase_names['Kentucky Fried Chicken'] = 'KFC'
+        fatabase_names["McDonald's"] = 'McDonalds'
+        fatabase_names["Milestone's"] = 'Milestones'
+        fatabase_names["Moxie's"] = 'Moxies'
+        fatabase_names['Mr Sub'] = 'MrSub'
+        fatabase_names["Wendy's"] = 'Wendys'
+
 
     with open('healthyfamiliesbc.csv') as hfbc:
         reader = csv.reader(hfbc)
         reader.next()
         hfbc_names = set(row[-1].strip() for row in reader)
+        hfbc_names = dict((n, n) for n in hfbc_names)
 
     with open('supermarkets.txt') as supermarkets:
         supermarket_names = set(line.strip() for line in supermarkets)
@@ -29,21 +44,21 @@ def main():
     # if fatabase_names & hfbc_names:
     #     raise Exception('overlapping names: ' + fatabase_names & hfbc_names)
 
-    if fatabase_names & menus:
+    if set(fatabase_names.keys()) & menus:
         raise Exception('overlapping menu: ' + fatabase_names & menus)
 
-    if hfbc_names & menus:
+    if set(hfbc_names.keys()) & menus:
         raise Exception('overlapping menu: ' + hfbc_names & menus)
 
     def find_menu(*candidates):
-        for name in hfbc_names:
+        for name, menu in hfbc_names.iteritems():
             for candidate in candidates:
                 if name in candidate:
-                    return name, candidate, "restaurant"
-        for name in fatabase_names:
+                    return menu, candidate, "restaurant"
+        for name, menu in fatabase_names.iteritems():
             for candidate in candidates:
                 if name in candidate:
-                    return name, candidate, "restaurant"
+                    return menu, candidate, "restaurant"
         for name in supermarket_names:
             for candidate in candidates:
                 if name in candidate:
